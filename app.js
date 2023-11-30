bc951b5fe7bea2faa20163ecf71ce78413029e44
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-30 18:50:08 +0530

    added success code and header

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ const server = http.createServer(function(req, res) {
     let path = req.url;
 
     if (path === '/' || path.toLowerCase() === 'home'){
+        res.writeHead(200, {
+            'Content-Type' : 'text/html'
+        })
         res.end(home);
     } else if (path.toLowerCase() === '/form') {
+        res.writeHead(200, {
+            'Content-Type' : 'text/html'
+        })
         res.end(form);
     }else {
+        res.writeHead(404, {
+            'Content-Type' : 'text/html'
+        })
         res.end('error 404 : Page not found');
     }
 })
