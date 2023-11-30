3ba078dc8c2c8950d92de1ea8a55ed13051aff79
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-30 16:24:08 +0530

    ussed router to create home and form pages,also git ignore to nodemon files

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const fs = require('fs');
 const http = require('http');
 const { type } = require('os');
 
-const html = fs.readFileSync('./index.html');
+const home = fs.readFileSync('./index.html');
+const form = fs.readFileSync('./form.html');
 
 const server = http.createServer(function(req, res) {
-    res.setHeader('Content-Type','text/html');
-    res.write(html);
-    res.end();
-    // console.log('A new requst is received');
+    let path = req.url;
+
+    if (path === '/' || path.toLowerCase() === 'home'){
+        res.end(home);
+    } else if (path.toLowerCase() === '/form') {
+        res.end(form);
+    }else {
+        res.end('error 404 : Page not found');
+    }
 })
 
 server.listen(8000,'127.0.0.1', () => {
