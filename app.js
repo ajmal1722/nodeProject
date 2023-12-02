23d27f08bd0de50cad765716d026157cd03bfee3
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-12-02 13:22:05 +0530

    created a table in server

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const { type } = require('os');
 
 const home = fs.readFileSync('./index.html', 'utf-8');
 const form = fs.readFileSync('./form.html', 'utf-8');
+const tableTemplate = fs.readFileSync('./table.html', 'utf-8')
 const jsonData = JSON.parse(fs.readFileSync('./Datas/data.json', 'utf-8'))
+
+const generateTableRows = () => {
+    return  jsonData.map(entry => {
+        return  `<tr>
+                    <td>${entry.no}</td>
+                    <td>${entry.name}</td>
+                    <td>${entry.age}</td>
+                    <td>${entry.email}</td>
+                    <td>${entry.gender}</td>
+                </tr>`
+    }).join('')
+            
+}
 
 const server = http.createServer(function(req, res) {
     let path = req.url;
@@ -14,7 +28,8 @@ const server = http.createServer(function(req, res) {
             'Content-Type' : 'text/html'
             // 'Content-Type' : 'application/json'
         })
-        res.end(home.replace('{{%CONTENT%}}', jsonData));
+        const tableContent = tableTemplate.replace('{{%TABLE_BODY%}}', generateTableRows())
+        res.end(home.replace('{{%TABLE_CONTENT%}}', tableContent));
     } else if (path.toLowerCase() === '/form') {
         res.writeHead(200, {
             'Content-Type' : 'text/html'
