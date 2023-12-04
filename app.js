35fccee219cba6ec44a3ba94ff072eefb3969ce7
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-12-04 12:39:41 +0530

    made the submit button works

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const http = require('http');
-const { type } = require('os');
+const querystring = require('querystring')
 
 const home = fs.readFileSync('./index.html', 'utf-8');
 const form = fs.readFileSync('./form.html', 'utf-8');
@@ -14,6 +14,7 @@ const generateTableRows = () => {
                     <td>${entry.name}</td>
                     <td>${entry.age}</td>
                     <td>${entry.phone}</td>
+
                     <td>${entry.email}</td>
                     <td>${entry.gender}</td>
                 </tr>`
@@ -23,20 +24,37 @@ const generateTableRows = () => {
 
 const server = http.createServer(function(req, res) {
     let path = req.url;
-
-    if (path === '/' || path.toLowerCase() === '/home?' || path.toLowerCase() === '/home'){
+    console.log(path)
+    if (path === '/' || path === '/home?' || path === '/home'){
         res.writeHead(200, {
             'Content-Type' : 'text/html'
             // 'Content-Type' : 'application/json'
         })
         const tableContent = tableTemplate.replace('{{%TABLE_BODY%}}', generateTableRows())
         res.end(home.replace('{{%TABLE_CONTENT%}}', tableContent));
-    } else if (path.toLowerCase() === '/form?' || path.toLowerCase() === '/form') {
+
+    } else if (path === '/form?' || path === '/form') {
         res.writeHead(200, {
             'Content-Type' : 'text/html'
         })
         res.end(form);
-    }else {
+
+    } else if (path === '/submit' || path === '/submit?'){
+        console.log("req.method");
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+        req.on('end',() => {
+            const formData = querystring.parse(body);
+
+            console.log('form data :',formData);
+
+            res.writeHead(200, {'Content-Type' : 'text-plain'});
+            res.end('Form submission Successful');
+        })
+
+    } else {
         res.writeHead(404, {
             'Content-Type' : 'text/html'
         })
